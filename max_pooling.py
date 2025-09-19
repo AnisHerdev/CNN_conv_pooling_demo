eaa8418860a8dbf5a2cbb46abbb271e7f8aec104
AnisHerdev <88375763+AnisHerdev@users.noreply.github.com>
2025-09-19 23:17:19 +0530

    Fixed bug in max pooling with the stride

--- max_pooling.py
+++ max_pooling.py
@@ -17,8 +17,21 @@ def max_pooling(img_array,filter_width=2,filter_height=2,stride=1):
     pooled_array = np.zeros((out_h,out_w))
     for i in range(out_h):
         for j in range(out_w):
-            segment = img_array[i:i+filter_height,j:j+filter_width]
+            segment = img_array[i*stride:i*stride+filter_height,j*stride:j*stride+filter_width]
             pooled_array[i,j]= segment.max()
+    return pooled_array
+
+def min_pooling(img_array,filter_width=2,filter_height=2,stride=1):
+    h,w = img_array.shape[:2]
+    out_h = (h - filter_height)//stride + 1
+    out_w = (w - filter_width)//stride + 1
+    print("Input:",h,w)
+    print("Output:",out_h,out_w)
+    pooled_array = np.zeros((out_h,out_w))
+    for i in range(out_h):
+        for j in range(out_w):
+            segment = img_array[i*stride:i*stride+filter_height,j*stride:j*stride+filter_width]
+            pooled_array[i,j]= segment.min()
     return pooled_array
 
 
@@ -35,8 +48,8 @@ if __name__ == "__main__":
     pooled_img = Image.fromarray(pooled_array_uint8)
     pooled_img.save("max_pooled.png")
     for i in range(layers):
-        pooled_array = max_pooling(img_array)
+        pooled_array = max_pooling(img_array,2,2,2)
         pooled_array_uint8 = pooled_array.astype(np.uint8)  # Convert to 8-bit unsigned int
         pooled_img = Image.fromarray(pooled_array_uint8)
         pooled_img.save(f"max_pooled_{i}.png")
-        img_array = pooled_array_uint8
+        img_array = pooled_array
