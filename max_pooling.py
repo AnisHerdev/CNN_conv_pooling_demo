a69a6fb82039e2b1a8739612845fdd47d8f6f2d3
AnisHerdev <88375763+AnisHerdev@users.noreply.github.com>
2025-09-19 22:55:50 +0530

    added code to perform max pooling

--- max_pooling.py
+++ max_pooling.py
@@ -1,35 +1,42 @@
 from PIL import Image
 import numpy as np
-def convert_to_numpy(file_name,width=100,height=100):
-    arr=[]
-    with Image.open(file_name) as img:
-        img = img.resize((width,height))
-        img = img.convert("1")
-        print(np.array(img))
-        pixels = img.load()
-    return arr
-# with Image.open("naruto.png") as img:
-#     img = img.resize((100,100))
-#     width,height= img.size
-#     print(img.size)
-#     # img = img.rotate(30)
-#     # img = img.crop((0,0,400,400))
-#     # img = img.resize((width*2,height*2))
-#     # print(img.histogram())
-#     print(img.mode)
-#     img_bw = img.convert("1")  # Convert to 1-bit pixels
-#     img = img.convert("1")  # Convert to 1-bit pixels
-#     # print(img_bw.tobitmap())
-#     for y in range(height):
-#         for x in range(width):
-#             print("1" if img.getpixel((x,y))==255 else "_" ,end="")
-#         print()
-#     img.save("rdd.png")  
+def convert_to_numpy(file_name,width=0,height=0):
+    with Image.open(file_name).convert("RGB") as img: 
+        if width and height:
+            img = img.resize((width,height))
+        # img = img.convert("1")
+        return np.array(img)
 
-def print_image(arr):
-    for y in arr:
-        print("".join(str(x) for x in y))
 
+def max_pooling(img_array,filter_width=2,filter_height=2,stride=1):
+    h,w = img_array.shape[:2]
+    out_h = (h - filter_height)//stride + 1
+    out_w = (w - filter_width)//stride + 1
+    print("Input:",h,w)
+    print("Output:",out_h,out_w)
+    pooled_array = np.zeros((out_h,out_w))
+    for i in range(out_h):
+        for j in range(out_w):
+            segment = img_array[i:i+filter_height,j:j+filter_width]
+            pooled_array[i,j]= segment.max()
+    return pooled_array
+
+
+layers = 5
 if __name__ == "__main__":
-    arr = convert_to_numpy("naruto.png")
-    print_image(arr)
+    img_array = convert_to_numpy("naruto.png")
+    # img_array = np.array([[1,2,3,4,5,6,6],[23,34,45,56,57,76,7],[23,32,23,54,65,75,65]])
+    # print(img_array)
+    pooled_array = max_pooling(img_array)
+    # print(pooled_array)
+    # print(pooled_array.shape)
+    # pooled_img = Image.fromarray(pooled_array)
+    pooled_array_uint8 = pooled_array.astype(np.uint8)  # Convert to 8-bit unsigned int
+    pooled_img = Image.fromarray(pooled_array_uint8)
+    pooled_img.save("max_pooled.png")
+    for i in range(layers):
+        pooled_array = max_pooling(img_array)
+        pooled_array_uint8 = pooled_array.astype(np.uint8)  # Convert to 8-bit unsigned int
+        pooled_img = Image.fromarray(pooled_array_uint8)
+        pooled_img.save(f"max_pooled_{i}.png")
+        img_array = pooled_array_uint8
